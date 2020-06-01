4cafd16af465dfd77a9e58c2c993eaa23f4afcd6
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-01 13:19:47 -0700

    add sub, mult, div functions in js
    co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,30 @@ const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 const result = add(number1, number2);
 alert(result);
+
+const sub = function(number1, number2) {
+  return number1 - number2;
+};
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = sub(number1, number2);
+alert(result);
+
+const mult = function(number1, number2) {
+  return number1 * number2;
+};
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = mult(number1, number2);
+alert(result);
+
+const div = function(number1, number2) {
+  return number1 / number2;
+};
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = div(number1, number2);
+alert(result);
