adec897cbca007340d449e47608a9293b3dde927
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-01 13:15:25 -0700

    add html boilerplate and js add function
    co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const add = function(number1, number2) {
+  return number1 + number2;
+};
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = add(number1, number2);
+alert(result);
