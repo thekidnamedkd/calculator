64bf8d721e319af043bc34dc20c2fea1c81e7958
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-03 16:03:21 -0700

    remove pop up prompts and alert result in favor of html output.
    Co-authored-by: Michael Petersen <michael_petersen_email@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
-//begin addition function
 
 const add = function(number1, number2) {
   return number1 + number2;
 };
 
-const add1 = parseInt(prompt("Enter a number:"));
-const add2 = parseInt(prompt("Enter another number:"));
-const addResult = add(add1, add2);
-alert(addResult);
+const subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+const multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+const divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+  event.preventDefault();
+  const number1 = parseInt($("#add1").val());
+  const number2 = parseInt($("#add2").val());
+  const result = add(number1, number2);
+  $("#output").text(result);
+  });
+});
 
 // //begin subtration function
 
