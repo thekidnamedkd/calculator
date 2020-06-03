94c3c281d3fb0d51901281a912f48cf343fc5160
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-03 15:43:02 -0700

    add forms and buttons to html for addition function.
    Co-authored-by: Michael Petersen <michael_petersen_email@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -9,35 +9,35 @@ const add2 = parseInt(prompt("Enter another number:"));
 const addResult = add(add1, add2);
 alert(addResult);
 
-//begin subtration function
+// //begin subtration function
 
-const sub = function(number1, number2) {
-  return number1 - number2;
-};
+// const sub = function(number1, number2) {
+//   return number1 - number2;
+// };
 
-const sub1 = parseInt(prompt("Enter a number:"));
-const sub2 = parseInt(prompt("Enter another number:"));
-const subResult = sub(sub1, sub2);
-alert(subResult);
+// const sub1 = parseInt(prompt("Enter a number:"));
+// const sub2 = parseInt(prompt("Enter another number:"));
+// const subResult = sub(sub1, sub2);
+// alert(subResult);
 
-//begin multiplication function
+// //begin multiplication function
 
-const mult = function(number1, number2) {
-  return number1 * number2;
-};
+// const mult = function(number1, number2) {
+//   return number1 * number2;
+// };
 
-const mult1 = parseInt(prompt("Enter a number:"));
-const mult2 = parseInt(prompt("Enter another number:"));
-const multResult = mult(mult1, mult2);
-alert(multResult);
+// const mult1 = parseInt(prompt("Enter a number:"));
+// const mult2 = parseInt(prompt("Enter another number:"));
+// const multResult = mult(mult1, mult2);
+// alert(multResult);
 
-//begin division function
+// //begin division function
 
-const div = function(number1, number2) {
-  return number1 / number2;
-};
+// const div = function(number1, number2) {
+//   return number1 / number2;
+// };
 
-const div1 = parseInt(prompt("Enter a number:"));
-const div2 = parseInt(prompt("Enter another number:"));
-const divResult = div(div1, div2);
-alert(divResult);
+// const div1 = parseInt(prompt("Enter a number:"));
+// const div2 = parseInt(prompt("Enter another number:"));
+// const divResult = div(div1, div2);
+// alert(divResult);
